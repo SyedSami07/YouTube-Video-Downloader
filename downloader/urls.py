36ddade1619b2345e5a1f75feef8889939a3af23
Syed Salman Sami <syedsamigithub@gmail.com>
2025-01-19 21:13:00 +0600

    Update urls.py

--- downloader/urls.py
+++ downloader/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.index, name='index'),
     path('download/', views.download_video, name='download_video'),
     path('about/',views.about, name='about'),
 ]
